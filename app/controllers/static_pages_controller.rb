9a3443497e4a8ece87e894f8baa5c80c64f79181
40grivenprog <grivenmus40@yandex.ru>
2019-07-24 16:12:50 +0300

    static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,7 @@
 class StaticPagesController < ApplicationController
+    def help
+    end
+    
+    def about 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
